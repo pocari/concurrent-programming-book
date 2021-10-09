02fae8c5e38121c517ae6eb34da37fb141ac518a
pocari <caffelattenonsugar@gmail.com>
2021-10-09 13:28:37 +0900

    デッドロックのパターンメモ

--- 4/rwlock/src/main.rs
+++ 4/rwlock/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     let val = Arc::new(RwLock::new(true));
 
     let th = thread::spawn(move || {
+        //  これだとデッドロックになる
+        // let flag = val.read().unwrap();
+        // if *flag {
+
         let flag = *val.read().unwrap();
         if flag {
             *val.write().unwrap() = false;

940bd9c681f8cd028662476d503caa2d99a809ba
pocari <caffelattenonsugar@gmail.com>
2021-10-17 14:11:49 +0900

    signalをうけるようのthreadをつくるサンプル rust版

---
+++ 4/4.6/signal1_rsut/src/main.rs
@@ -0,0 +1,17 @@
+use std::{error::Error, process, thread, time::Duration};
+
+use signal_hook::{consts::SIGUSR1, iterator::Signals};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("pid: {}", process::id());
+
+    let mut signals = Signals::new(&[SIGUSR1])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("received signal: {:?}", sig);
+        }
+    });
+
+    thread::sleep(Duration::from_secs(10));
+    Ok(())
+}
